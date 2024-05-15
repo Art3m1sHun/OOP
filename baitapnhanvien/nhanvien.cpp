0de1c03a854f80ce63dd265e7285713b13b4fe64
Art3m1sHun <minhhungdenguyn052@gmail.com>
2024-05-15 15:04:41 +0700

    update

--- baitapnhanvien/nhanvien.cpp
+++ baitapnhanvien/nhanvien.cpp
@@ -1,34 +1,111 @@
-#include"nhanvien.h"
-
-NhanVien::NhanVien()
-{
-
-}
-
-NhanVien::~NhanVien()
-{
-
-}
+#include"NhanVien.h"
 
 void NhanVien::Nhap()
 {
-    cout << "Nhap ho va ten: ";
-    cin >> HoTen;
-    cout << "Nhap ngay sinh: ";
-    cin >> Ngay;
-    cout << "Nhap thang sinh: ";
-    cin >> Thang;
-    cout << "Nhap nam sinh: ";
-    cin >> Nam;
+    cout << "Nhap Ho Va Ten Nhan Vien: ";
+    cin.ignore();
+    getline(cin, Name);
+    cout << "Nhap Ngay Sinh Nhan Vien: ";
+    cin.ignore();
+    getline(cin, NgaySinh);
+}
+void NhanVien::Xuat() const
+{
+    cout << "Ho va Ten: " << Name << endl;
+    cout << "Ngay Sinh: " << NgaySinh << endl;
 }
 
-void NhanVien::Xuat()
+void NhanVienSanXuat::Nhap()
 {
-    cout << "Ho va ten nhan vien: " << HoTen << endl;
-    cout << "Ngay thang nam sinh: " << Ngay << "/" << Thang << "/" << Nam << endl;
+    NhanVien::Nhap();
+    cout << "Nhap Luong Co Ban: ";
+    cin >> LuongCoBan;
+    cout << "Nhap So San Pham: ";
+    cin >> SoSanPham;
+}
+void NhanVienSanXuat::Xuat() const
+{
+    NhanVien::Xuat();
+    cout << "Luong Co Ban: " << LuongCoBan << endl;
+    cout << "So San Pham: " << SoSanPham << endl;
+}
+void NhanVienSanXuat::TinhLuong()
+{
+    this->Luong = LuongCoBan + SoSanPham * 5000;
 }
 
-long NhanVien::Luong()
+void NhanVienVanPhong::Nhap()
 {
-    return 0;
+    NhanVien::Nhap();
+    cout << "Nhap So Ngay Lam Viec: ";
+    cin >> SoNgayLamViec;
+}
+void NhanVienVanPhong::Xuat() const
+{
+    NhanVien::Xuat();
+    cout << "So Ngay Lam Viec: " << SoNgayLamViec;
+}
+void NhanVienVanPhong::TinhLuong()
+{
+    this->Luong = SoNgayLamViec * 100000.0;
+}
+
+void QuanLyNV::Nhap()
+{
+    cout << "Nhap So Luong Nhan Vien San Xuat: ";
+    cin >> SoLuongNVSX;
+    DSNVSX = new NhanVienSanXuat[SoLuongNVSX];
+    for (int i = 0; i < SoLuongNVSX; i++)
+    {
+        DSNVSX[i].Nhap();
+    }
+    cout << "Nhap So Luong Nhan Vien Van Phong: ";
+    cin >> SoLuongNVVP;
+    DSNVVP = new NhanVienVanPhong[SoLuongNVVP];
+    for (int i = 0; i < SoLuongNVVP; i++)
+    {
+        DSNVVP[i].Nhap();
+    }
+}
+void QuanLyNV::Xuat() const
+{
+    cout << "Xuat Nhan Vien San Xuat: " << endl;
+    for (int i = 0; i < SoLuongNVSX; i++)
+    {
+        DSNVSX[i].Xuat();
+    }
+    cout << "Xuat Nhan Vien Van Phong: " << endl;
+    for (int i = 0; i < SoLuongNVVP; i++)
+    {
+        DSNVVP[i].Xuat();
+    }
+}
+void QuanLyNV::TinhLuong()
+{
+    for (int i = 0; i < SoLuongNVSX; i++)
+    {
+        DSNVSX[i].TinhLuong();
+    }
+    for (int i = 0; i < SoLuongNVVP; i++)
+    {
+        DSNVVP[i].TinhLuong();
+    }
+}
+void QuanLyNV::TinhTongLuong()
+{
+    long double TongLuong = 0;
+    for (int i = 0; i < SoLuongNVSX; i++)
+    {
+        TongLuong += DSNVSX[i].getLuong();
+    }
+    for (int i = 0; i < SoLuongNVVP; i++)
+    {
+        TongLuong += DSNVVP[i].getLuong();
+    }
+    cout << "Tong Luong Cong Ty Phai Tra Cho Nhan Vien La: " << TongLuong << endl;
+}
+QuanLyNV::~QuanLyNV()
+{
+    delete[] DSNVSX;
+    delete[] DSNVVP;
 }
--- baitapnhanvien/nhanvien.h
+++ baitapnhanvien/nhanvien.h
@@ -5,13 +5,72 @@ using namespace std;
 
 class NhanVien
 {
-protected:
-    char HoTen[100];
-    int Ngay, Thang, Nam;
 public:
-    NhanVien();
-    ~NhanVien();
+    NhanVien()
+    {
+        Name = "Unknows";
+        NgaySinh = "Unknows";
+        Luong = 0;
+    }
     void Nhap();
-    void Xuat();
-    long Luong();
+    void Xuat() const;
+    void TinhLuong();
+    float getLuong()
+    {
+        return Luong;
+    }
+protected:
+    string Name;
+    string NgaySinh;
+    float Luong;
+private:
+};
+
+class NhanVienSanXuat : public NhanVien
+{
+public:
+    NhanVienSanXuat()
+    {
+        LuongCoBan = 0;
+        SoSanPham = 0;
+    }
+    void Nhap();
+    void Xuat() const;
+    void TinhLuong();
+protected:
+    float LuongCoBan;
+    int SoSanPham;
+private:
+};
+
+class NhanVienVanPhong : public NhanVien
+{
+public:
+    NhanVienVanPhong()
+    {
+        SoNgayLamViec = 0;
+    }
+    void Nhap();
+    void Xuat() const;
+    void TinhLuong();
+protected:
+    int SoNgayLamViec;
+private:
+};
+
+class QuanLyNV
+{
+public:
+    void Nhap();
+    void Xuat() const;
+    void TinhLuong();
+    void TinhTongLuong();
+    ~QuanLyNV();
+protected:
+
+private:
+    int SoLuongNVSX;
+    int SoLuongNVVP;
+    NhanVienSanXuat* DSNVSX;
+    NhanVienVanPhong* DSNVVP;
 };
